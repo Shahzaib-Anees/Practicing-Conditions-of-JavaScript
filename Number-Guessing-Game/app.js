01c223ba254d56de4ffb17a07efc1bdb8e6b67dc
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-03-05 22:43:54 +0500

    Changes in alerts in Guess game Function

--- Number-Guessing-Game/app.js
+++ Number-Guessing-Game/app.js
@@ -2,9 +2,9 @@ function guess_game(){
     let mynum = 7;
     let user_guess = +prompt("Guess the number between 1 to 10 :");
     if(user_guess == mynum){
-       alert(`Congratulations! You have guessed the correct number`)
+       alert(`Bingo! Correct answer`)
     }else if (user_guess == 5 ||user_guess == 6){
-        alert(`Oops! You are very closed`)
+        alert(`Close enough to the correct answer`)
     }else{
         alert(`Your guess is Wrong`);
     }
