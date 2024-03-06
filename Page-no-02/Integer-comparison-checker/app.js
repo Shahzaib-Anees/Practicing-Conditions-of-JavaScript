776abd753a9778391a9c95dfba95187076fca9e0
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-03-06 23:52:29 +0500

    modification in Integer comparator added

--- Page-no-02/Integer-comparison-checker/app.js
+++ Page-no-02/Integer-comparison-checker/app.js
@@ -1,6 +1,8 @@
 function compare_integer(){
     let userValue1 = +prompt("Enter integer one");
-    let userValue2 = +prompt("Enter integer two");
+      console.log(userValue1);
+      let userValue2 = +prompt("Enter integer two");
+      console.log(userValue2);
     if(typeof(userValue1) == "number" && typeof(userValue2) == "number"){
         if(userValue1 > userValue2){
             alert(`${userValue1} is greater than ${userValue2}`);
@@ -8,6 +10,7 @@ function compare_integer(){
             alert(`${userValue2} is greater than ${userValue1}`);
         }
     }else{
-        console.log("Error")
+        console.log("Error! Please enter only numbers/integers")
+        alert("Error! Please enter only numbers/integers")
     }
 }
