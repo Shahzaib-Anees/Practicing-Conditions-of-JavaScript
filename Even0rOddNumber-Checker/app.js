65671d92daa564b83f7dd5d1b034258ff905f590
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-03-06 22:11:32 +0500

    Some Changes made to enhance Even or odd Number Checker

--- Even0rOddNumber-Checker/app.js
+++ Even0rOddNumber-Checker/app.js
@@ -1,9 +1,9 @@
 let user_input_number = +prompt("Enter a number to check whether it is Even or Odd;");
 let out_put = user_input_number % 2;
 if(out_put == 0){
-    alert("The Number is Even")
+    alert(`${user_input_number} is an Even Number`);
 }else if(out_put > 0){
-    alert("The Number is Odd")
+    alert(`${user_input_number} is an Odd Number`);
 }else{
     alert("Unknown Error")
 }
