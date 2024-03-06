b65c0266ac2fbb48fcf8ed84aeca1b5de95b4ad8
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-03-06 23:30:16 +0500

    Program Checked and excuted successully

--- Page-no-02/UserInput-Case-&_Number-Checker/app.js
+++ Page-no-02/UserInput-Case-&_Number-Checker/app.js
@@ -1,13 +1,12 @@
 function input_checker(){
     let userInputValue = prompt("Enter an English Alphabet/Letter or a Number");
     let stringCase = userInputValue.charCodeAt(userInputValue);
-    console.log(stringCase);
-    let numberCase = userInputValue.parseInt;
+    let numberCase = parseInt(userInputValue);
     if(stringCase == 65 || stringCase <=90){
         alert(`The ${userInputValue} is a UpperCase String`);
     }else if (stringCase == 97 || stringCase <=122){
         alert(`The ${userInputValue} is a LowerCase String`);
-    }else if(typeof(numberCase) !== Number){
+    }else if(typeof(numberCase) == "number"){
         alert(`The ${userInputValue} is a Number`);
     }else {
         alert(`UnKnown Error caught`);
