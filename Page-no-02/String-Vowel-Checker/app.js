a9841063eeadf61501e106d304e5655c20c45f98
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-03-07 00:15:06 +0500

    User Input Viwel Checker Added

---
+++ Page-no-02/String-Vowel-Checker/app.js
@@ -0,0 +1,12 @@
+function vowel_checker(){
+    let user_string = prompt("Enter a Letter/Alphabet");
+    if(user_string.length === 1){
+        if(user_string === "a" || user_string === "A"|| user_string === "e"||user_string === "E" || user_string === "i"||user_string === "I"  || user_string === "o"||user_string === "O" || user_string==="u"||user_string === "U" ){
+            alert(`Your input letter is a Vowel`)
+        }else{
+            alert(`Your input letter is not a Vowel`)
+        }
+    }else{
+        alert(`Only one Character is allowed`)
+    }
+}
