5ba86f40aab582e269ce07d6816936959f9a2e3f
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-03-07 22:43:23 +0500

    Modifying User Geeter w.r.t time

--- Page-no-02/user-time-greeter/app.js
+++ Page-no-02/user-time-greeter/app.js
@@ -1,12 +1,12 @@
 let user_input_time = +prompt("Enter your Local Time in 24 hours Format :");
 if (user_input_time >= 0 && user_input_time <= 2400) {
-    if (user_input_time === 0 || user_input_time < 1200) {
+    if (user_input_time === 0 && user_input_time < 1200) {
         alert(`Good Morning!`)
-    } else if (user_input_time >= 1200 || user_input_time < 1700 ) {
+    } else if (user_input_time >= 1200 && user_input_time < 1700 ) {
         alert(`Good Afternoon!`)
-    } else if (user_input_time >= 1700  || user_input_time < 2100 )  {
+    } else if (user_input_time >= 1700  && user_input_time < 2100 )  {
         alert(`Good Evening!`)
-    } else if (user_input_time >= 2100 || user_input_time <= 2359) {
+    } else if (user_input_time >= 2100 && user_input_time <= 2359) {
         alert(`Good Night!`)
     } else {
         alert(`Error!`)
